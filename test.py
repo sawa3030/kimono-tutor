772d3ae1937fb80c3864715edb590f871c39c89b
sawa3030 <esawada2003@gmail.com>
2025-02-20 21:28:34 +0900

    format the code

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
+import os
+import pickle
+
+import matplotlib.pyplot as plt
 import torch
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
 from torch import nn
 from torch.utils.data import DataLoader
-import torchvision
 from torchvision import datasets
-import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-from torchvision.transforms import ToTensor, Lambda, Compose
-from PIL import Image
-import matplotlib.pyplot as plt
-import os
-from common import transform
-from common import NeuralNetwork
-import pickle
+from torchvision.transforms import Compose, Lambda, ToTensor
+
+from common import NeuralNetwork, transform
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
@@ -29,12 +30,12 @@ classes = [
 
 model.eval()
 
-image_path = os.path.expanduser('~/kimono/train_data/cross/image.png')
-image = Image.open(image_path).convert("RGB") 
+image_path = os.path.expanduser("~/kimono/train_data/cross/image.png")
+image = Image.open(image_path).convert("RGB")
 image = transform(image).unsqueeze(0).to(device)
 
 
-with open('class_to_idx.pickle.pkl', 'rb') as f:
+with open("class_to_idx.pickle.pkl", "rb") as f:
     class_to_idx = pickle.load(f)
     idx_to_class = {v: k for k, v in class_to_idx.items()}
     with torch.no_grad():
--- train.py
+++ train.py
@@ -1,29 +1,32 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-import torchvision
-from torchvision import datasets
-import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-from torchvision.transforms import ToTensor, Lambda, Compose
-import matplotlib.pyplot as plt
 import os
-from common import transform
-from common import NeuralNetwork
 import pickle
 
-training_data = ImageFolder(root=os.path.expanduser('~/kimono/train_data/'), transform=transform)
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import Compose, Lambda, ToTensor
+
+from common import NeuralNetwork, transform
+
+training_data = ImageFolder(
+    root=os.path.expanduser("~/kimono/train_data/"), transform=transform
+)
 train_dataloader = DataLoader(training_data, batch_size=10, shuffle=True)
 test_dataloader = DataLoader(training_data, batch_size=10, shuffle=True)
 
-with open('class_to_idx.pickle.pkl', 'wb') as f:
+with open("class_to_idx.pickle.pkl", "wb") as f:
     pickle.dump(training_data.class_to_idx, f)
 
 for X, y in test_dataloader:
     print("Shape of X [N, C, H, W]: ", X.shape)
     print("Shape of y: ", y.shape, y.dtype)
     break
-    
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
 
@@ -31,15 +34,16 @@ model = NeuralNetwork().to(device)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
+
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
-        
+
         # 損失誤差を計算
         pred = model(X)
         loss = loss_fn(pred, y)
-        
+
         # バックプロパゲーション
         optimizer.zero_grad()
         loss.backward()
@@ -48,6 +52,7 @@ def train(dataloader, model, loss_fn, optimizer):
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
 
 def test(dataloader, model):
     size = len(dataloader.dataset)
@@ -61,7 +66,10 @@ def test(dataloader, model):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= size
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
+
 
 epochs = 5
 for t in range(epochs):
