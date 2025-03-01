0dd002e39c697c27d19af72d3d70feef8cd6471d
sawa3030 <esawada2003@gmail.com>
2025-03-01 16:06:38 +0900

    refactor code

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-from ultralytics import YOLO
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 import torch
-
-# model = YOLO('./runs/train/weights/best.pt')
-# print(model)
-# results = model("data/train/images/DSC_3239.JPG")
-# # results = model("./data/test/images/y17_jpg.rf.1134f8afd5cda5d2a789b0474d537baa.jpg")
-# results[0].show()
+from matplotlib import pyplot as plt
+from ultralytics import YOLO
 
 model = YOLO("yolov8x-seg.pt")
 results = model.predict("input.jpg", save=True, show=True)
