27fd86696fbe28797ce8faa4f4223f292bf29c85
sawa3030 <esawada2003@gmail.com>
2025-03-16 17:39:04 +0900

    take picture

---
+++ kimono-tutor/app/index.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+
+export default function Index() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [cameraRef, setCamera] = useState<CameraView | null>(null);
+  const [picture, setPicture] = useState(null);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      console.log(photo);
+      console.log("took photo");
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} ref={(ref) =>{
+        setCamera(ref);
+      }}>
+        <View>
+          <TouchableOpacity onPress={toggleCameraFacing}>
+            <Text>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={takePicture}>
+            <Text>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+});
