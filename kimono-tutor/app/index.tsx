a346212228e939367cb95bdd2625de3e364b9afc
sawa3030 <esawada2003@gmail.com>
2025-03-21 08:33:25 +0900

    simplify the code

--- kimono-tutor/app/index.tsx
+++ kimono-tutor/app/index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
   const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
   const [cameraRef, setCamera] = useState<CameraView | null>(null);
-  const [picture, setPicture] = useState<null | string>(null);
+  const [pictureURI, setPictureURI] = useState<null | string>(null);
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -48,46 +48,36 @@ export default function Index() {
         console.log("no photo");
         return;
       }
-      setPicture(photo.uri);
+      setPictureURI(photo.uri);
     }
   };
 
   function dataURItoBlob(dataURI: string) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
     var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
+    if (dataURI.split(",")[0].indexOf("base64") >= 0)
+      byteString = atob(dataURI.split(",")[1]);
+    else byteString = unescape(dataURI.split(",")[1]);
 
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
 
-    // write the bytes of the string to a typed array
     var ia = new Uint8Array(byteString.length);
     for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
+      ia[i] = byteString.charCodeAt(i);
     }
 
-    return new Blob([ia], {type:mimeString});
-}
+    return new Blob([ia], { type: mimeString });
+  }
 
   const postPicture = async () => {
-    if (picture) {
-      const data = { number: "1" };
+    if (pictureURI) {
       const formData = new FormData();
 
-      var blob = dataURItoBlob(picture);
-      formData.append("picture", blob)
+      var blob = dataURItoBlob(pictureURI);
+      formData.append("picture", blob);
 
       const uploadResponse = await fetch("http://127.0.0.1:8000", {
         method: "POST",
-        // headers: {
-        //   "Content-Type": "multipart/form-data",
-
-        // },
-        // body: JSON.stringify(data),
-        body : formData,
+        body: formData,
       });
       const result = await uploadResponse.json();
       console.log(result);
@@ -96,12 +86,12 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      {picture ? (
+      {pictureURI ? (
         <>
           <TouchableOpacity onPress={postPicture}>
             <Text>Post Picture</Text>
           </TouchableOpacity>
-          <Image source={{ uri: picture }} style={styles.camera} />
+          <Image source={{ uri: pictureURI }} style={styles.camera} />
         </>
       ) : (
         <CameraView
